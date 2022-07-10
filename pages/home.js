0d45380ab7775e8c8fa9b691948aed781e6cb0e5
Guirdo <produccionesaldo@gmail.com>
2022-07-09 19:42:20 -0500

    Pagination added to HomePage

--- pages/home.js
+++ pages/home.js
@@ -4,18 +4,20 @@ import Layout from '../components/Layout';
 import { useSelector } from "react-redux";
 import Link from "next/link";
 import { supabase } from "../utils/supabaseClient";
+import { DownRoundArrow } from "iconoir-react";
+import { useState } from "react";
 
 export async function getStaticProps() {
-    const { data:posts } = await supabase
+    const { data: posts } = await supabase
         .from('post')
         .select(`
             id,description,songlink,
             created_at,author, reported,
             profiles:author(username,avatar_url,banned)
         `)
-        .not('reported','eq', true)
+        .not('reported', 'eq', true)
         .order('created_at', { ascending: false })
-        .limit(15)
+        .range(0, 9)
 
     return {
         props: {
@@ -25,8 +27,33 @@ export async function getStaticProps() {
     };
 }
 
-function HomeScreen({posts}) {
+function HomeScreen({ posts }) {
     const { isAuthenticated } = useSelector(state => state.auth)
+    const [postHome, setPostHome] = useState(posts)
+    const [lastPost, setLastPost] = useState(9)
+    const [disable, setDisable] = useState(false)
+
+    const loadMorePosts = async () => {
+        const { data,count } = await supabase
+            .from('post')
+            .select(`
+                id,description,songlink,
+                created_at,author, reported,
+                profiles:author(username,avatar_url,banned)
+            `,{count: "exact"})
+            .not('reported', 'eq', true)
+            .order('created_at', { ascending: false })
+            .range(lastPost + 1, lastPost + 10)
+
+        if (data) {
+            setPostHome(postHome => [...postHome, ...data])
+            setLastPost(lastPost + 10)
+        }
+
+        if(postHome.length === count){
+            setDisable(true)
+        }
+    }
 
     return (
         <Layout
@@ -45,13 +72,23 @@ function HomeScreen({posts}) {
             </div>
 
             {
-                posts?.map(post => (
+                postHome?.map(post => (
                     <Post
                         key={post.id}
                         post={post}
                         profile={post.profiles}
                     />
                 ))
+            }
+
+            {
+                !disable && <div
+                    className="home-pagination"
+                    onClick={loadMorePosts}
+                >
+                    <span className="home-pagination__text">See more</span>
+                    <span><DownRoundArrow strokeWidth={2} /></span>
+                </div>
             }
 
         </Layout>
